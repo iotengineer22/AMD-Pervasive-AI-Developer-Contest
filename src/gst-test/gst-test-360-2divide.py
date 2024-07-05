7ed84793a0690305269409ac2245da275c2e2788
misoji engineer <48165496+iotengineer22@users.noreply.github.com>
2024-07-06 06:07:25 +0900

    Add files via upload

--- src/gst-test/gst-test-360-2divide.py
+++ src/gst-test/gst-test-360-2divide.py
@@ -16,10 +16,15 @@ if not cap.isOpened():
 else:
     print("The camera opened successfully.")
 
+
+# Initialize variables for Avg_FPS calculation
+frame_count = 0
+avg_start_time = time.time()
+
 while True:
     start_time = time.time()
+
     ret, frame = cap.read()
-    end_time = time.time()
     if not ret:
         break
 
@@ -30,7 +35,7 @@ while True:
     shift = width // 4
     frame_shifted = np.roll(frame, shift, axis=1)
 
-    # Split the shifted image into 4 sections
+    # Split the shifted image into 2 sections
     sections = [
         frame_shifted[:, :width // 2],                  #front
         frame_shifted[:, width // 2:]                  #back
@@ -40,14 +45,31 @@ while True:
     for i, section in enumerate(sections):
         cv2.imshow(f"Section {i+1}", section)
 
+    end_time = time.time()
     
     print("360 Camera caputure time: {:.4f} seconds".format(end_time - start_time))
     print("Camera Performance: {} FPS".format(1/(end_time - start_time)))
     print(" ")
 
+
+    # Update frame count
+    frame_count += 1
+
+    # Check the time every 100 frames
+    if frame_count % 100 == 0:
+        avg_end_time = time.time()
+        elapsed_time = avg_end_time - avg_start_time
+        fps = frame_count / elapsed_time
+        print(" ")
+        print("Avg_FPS:", fps)
+        print(" ")
+        # Reset the timer and frame count
+        frame_count = 0
+        avg_start_time = time.time()
+
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
 
 # Post-processing
 cap.release()
